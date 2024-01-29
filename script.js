36c349b00e612894b2508b2962a27aa098729448
Pedro Henrique Bortolini Rodrigues <89049144+PedroBortoliniRodrigues@users.noreply.github.com>
2024-01-29 17:54:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal(`#biografia`, {duration: 3000});
+
+sr.reveal(`#certificados`, {duration: 3000});
+
+sr.reveal(`#experiencias`, {duration: 3000});
+
+sr.reveal(`#contato`, {duration: 3000});
+
